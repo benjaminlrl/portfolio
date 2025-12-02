1a51ed56b8d74141e560ed3d950ade0e1737e449
Benjamin Lorieul <benjamin.lorieul.pro@gmail.com>
2025-12-02 21:37:37 +0100

    refactor: correction des routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/projets', 'Projets::index');
-$routes->get('/portefeuille_de_competences', 'Home::portefeuilleDeCompetences');
+$routes->get('/', 'HomeController::index');
+$routes->get('/portefeuille_de_competences', 'HomeController::portefeuilleDeCompetences');
